244de046ce986080458a3b2acd02b8d8f5426cba
RhymeLyet <rhymeinparentheses@gmail.com>
2017-09-17 12:55:59 -0700

    Churros

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		int broken = 0;
+		for(int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i) == "cracked") {
+				broken++;
+			}
+		}
+		return broken * 2; //<- this needs changing
+	}
+
+	public static Object countPearls(List<Boolean> oysters) {
+		int broken = 0;
+		for(int i = 0; i < oysters.size(); i++) {
+			if(oysters.get(i) == true) {
+				broken++;
+			}
+		}
+		return broken;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		
+		return 0;
 	}
 	
 	//Add other methods here
