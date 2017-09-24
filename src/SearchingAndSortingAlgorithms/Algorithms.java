ebd7ed0f1872b9410a1aaf9085db2da90e131442
RhymeLyet <rhymeinparentheses@gmail.com>
2017-09-24 12:56:26 -0700

    Orange Chicken?

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package SearchingAndSortingAlgorithms;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Algorithms {
@@ -24,9 +25,52 @@ public class Algorithms {
 	}
 
 	public static double findTallest(List<Double> peeps) {
-		
-		return 0;
+		Double tallest = 0.0;
+		for(int i = 0; i < peeps.size(); i++) {
+			if(tallest < peeps.get(i)) {
+				tallest = peeps.get(i);
+			}
+		}
+		return tallest;
 	}
+
+	public static String findLongestWord(List<String> words) {
+		String longest = "";
+		for(int i = 0; i < words.size(); i++) {
+			if(words.get(i).length() > longest.length()) {
+				longest = words.get(i);
+			}
+		}
+		return longest;
+	}
+
+	public static Boolean containsSOS(List<String> message1) {
+		boolean returner = false;
+		for(int i = 0; i < message1.size(); i++) {
+		if(message1.get(i).equals(" ... --- ... ")) {
+			returner = true;
+		}
+		}
+		return returner;
+	}
+	
+
+	
+
+	public static List<String> sortScores(List<Double> results) {
+		List<String> returnable = Arrays.asList(new String[] {"60.3", "60.2", "80.4", "67.2", "95.0", "85.5", "40.7", "68.2", "38.4", "94.2", "85.0", "84.5", "50.1", "66.6" });
+		
+		for(int i = 0; i < results.size(); i++) {
+			if(Integer.parseInt(returnable.get(i)) > Integer.parseInt(returnable.get(i + 1))) {
+				String temp = returnable.get(i + 1);
+				returnable.get(i + 1).equals(returnable.get(i));
+				returnable.get(i).equals(temp);
+			}
+		}
+		System.out.println(returnable);
+		return returnable;
+	}
+
 	
 	//Add other methods here
 }
